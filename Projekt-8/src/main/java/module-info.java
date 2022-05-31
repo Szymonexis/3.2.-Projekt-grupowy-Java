b3daf142d4d106549350feacc6489d024c055b10
Szymonexis <szymonkaszuba3@wp.pl>
2022-05-31 10:32:43 +0200

    update

---
+++ Projekt-8/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.projekt8 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javax.servlet.api;
+
+    opens com.example.projekt8 to javafx.fxml;
+    exports com.example.projekt8;
+}
